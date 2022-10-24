8fd15314e916941d12f32aabf974e47f2bff63e6
Tommy Rozgonyi <tommyrozgonyi@gmail.com>
2022-10-24 14:30:45 -0400

    API adjustment 3

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const withCSS = require('@zeit/next-css')
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/submitted",
-        permanent: false
-      },
-    ]
-  },
-}
 
 // next.config.js
 module.exports = {
