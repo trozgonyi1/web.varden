7cb5dc07930699d0bb2d07e30be1c67cc7417475
Tommy Rozgonyi <tommyrozgonyi@gmail.com>
2022-10-24 14:24:19 -0400

    API adjustment

--- pages/api/data/index.tsx
+++ pages/api/data/index.tsx
@@ -11,6 +11,17 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
   ) {
+
+    res.setHeader('Access-Control-Allow-Credentials', "true")
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+
     try {
         var sequelize = new Sequelize(process.env.DATABASE_URL, {
           dialectOptions: {
